7488dd5bbb83a7391cf7680ac0287f601807e582
David Disseldorp <ddiss@suse.de>
2017-01-29 13:31:50 +0100

    add --verify-revision-key parameter
    When provided, perform GPG verification of the given source revision.
    Currently only Git tag signature verification is supported - i.e.
    revision must directly correspond to a git tag, with a signature that
    was created using the key.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- tar_scm.py
+++ tar_scm.py
@@ -1208,11 +1208,18 @@ def parse_args():
     parser.add_argument('--history-depth',
                         help='Obsolete osc service parameter that does '
                              'nothing')
+    parser.add_argument('--verify-revision-key', default='',
+                        help='Specify GPG public key to use for verification'
+			     'of the source revision')
     args = parser.parse_args()
 
     # basic argument validation
     if not os.path.isdir(args.outdir):
         sys.exit("%s: No such directory" % args.outdir)
+
+    if (args.verify_revision_key
+                      and not os.path.isfile(args.verify_revision_key)):
+        sys.exit("%s: No such file" % args.verify_revision_key)
 
     args.outdir = os.path.abspath(args.outdir)
     orig_subdir = args.subdir
@@ -1335,6 +1342,9 @@ def singletask(use_obs_scm, args):
         repodir = os.getcwd()
 
     clone_dir = scm_object.fetch_upstream(out_dir=repodir, **args.__dict__)
+
+    if args.verify_revision_key:
+	    scm_object.verify_repo(clone_dir, args.verify_revision_key)
 
     if args.filename:
         dstname = basename = args.filename
